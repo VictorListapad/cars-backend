db40746ae727895caadb9017a8412347fa87a396
Victor Listapad <victorlistapadjob@gmail.com>
2021-11-23 14:33:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+require("dotenv").config();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to DB..."))
+  .catch(() => console.log("Couldn't connect to DB!"));
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log("Server running...");
+});
